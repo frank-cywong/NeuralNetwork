9d0b275163870d0612cdb825562d8af1fba72a6f
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2022-05-17 12:41:31 -0400

    Activation functions made static, added leaky relu

--- RectifiedLinearActivationFunction.java
+++ RectifiedLinearActivationFunction.java
@@ -1,8 +1,8 @@
 public class RectifiedLinearActivationFunction implements ActivationFunction{
-  public double compute(double v){
+  public static double compute(double v){
     return(v > 0 ? v : 0);
   }
-  public double compute_derivative(double v){
+  public static double compute_derivative(double v){
     return(v > 0 ? 1 : 0); // assume if value is 0, just use 0 as derivative
   }
 }
