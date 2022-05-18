f175aabb8242f4980400eb4a1a7f3dabd6621c3a
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2022-05-18 19:06:41 -0400

    Softmax compute done

--- Softmax.java
+++ Softmax.java
@@ -2,6 +2,20 @@ public class Softmax implements ActivationFunction{
   public double compute(double v){
     throw new UnsupportedOperationException("Softmax requires the entire vector to compute the output vector");
   }
+  // S(v)i = e^(vi) / (sum across all j (e^(vj)))
+  public double[] compute(double[] v){
+    double[] output = new double[v.length];
+    double[] exps = new double[v.length];
+    double sum = 0;
+    for(int i = 0; i < v.length; i++){
+      exps[i] = Math.exp(v[i]);
+      sum += exps[i];
+    }
+    for(int i = 0; i < v.length; i++){
+      output[i] = exps[i] / sum;
+    }
+    return output;
+  }
   public double compute_derivative(double v){
     throw new UnsupportedOperationException("Softmax requires the entire vector to compute the derivatives");
   }
